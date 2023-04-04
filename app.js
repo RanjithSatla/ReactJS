57828aba800accf3dc70617e28e6da76ae931032
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-04 11:57:43 +0530

    Imported React and ReactDOM

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement(
   "h1",
   { id: "abc" },
@@ -11,7 +13,18 @@ console.log(heading); //Object
 Object
  */
 
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Iam an h1 tag"),
+    React.createElement("h2", {}, "Iam an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Iam an h1 tag"),
+    React.createElement("h2", {}, "Iam an h2 tag"),
+  ]),
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
 
 //render is response to take the above react element which is a JS obj and creates an h1 tag and places into root.
