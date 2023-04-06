28c26d2c64ab513886809cfe6f48233837f639e9
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-06 12:04:14 +0530

    Component composition

--- Components/FunctionalComponent.js
+++ Components/FunctionalComponent.js
@@ -7,10 +7,16 @@ const element = <h1>This is the element</h1>;
 
 // Functional Component : A Functional component is a normal JS function which returns a JSX Element i.e React Element.
 
+const ChildComponent = () => {
+  return <h1>This is a ChildComponent</h1>;
+};
+
 const FunctionalComponent = () => {
   return (
     <div className="Container">
       <h1>This is a Functional Component</h1>
+      {/* Component Compostion : Composing a component in an another component */}
+      <ChildComponent />
       {/* Nesting an React Element in a Functional component */}
       {element}
     </div>
