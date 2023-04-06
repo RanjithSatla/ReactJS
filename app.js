20a3509c5024b8a1fbd2e4fc8b77f97dc7f1f6ec
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-06 10:09:20 +0530

    Created React Elements using both React.createElement & JSX

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+//React.createElement => JS Object => root.render => HTMLElement.
+
 const heading = React.createElement(
   "h1",
-  { id: "abc" },
-  "Hello world from React "
+  { id: "heading" },
+  "Namaste React using React.createElement"
 );
-console.log(heading); //Object
-
-/**
- Heading is an JS Object not an h1 tag : 
- {$$typeof: Symbol(react.element), type: 'h1', key: null, ref: null,props: {id: 'abc', children: 'Hello world from React '}, …}
-Object
- */
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Iam an h1 tag"),
-    React.createElement("h2", {}, "Iam an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "Iam an h1 tag"),
-    React.createElement("h2", {}, "Iam an h2 tag"),
-  ]),
-]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
 
-//render is response to take the above react element which is a JS obj and creates an h1 tag and places into root.
+//Creating React element using JSX.
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+
+//jsxHeading -> Babel(transpiles) => React.createElement => JS Obj => HTMLElement(render)
+
+// root.render(heading);
+root.render(jsxHeading);
