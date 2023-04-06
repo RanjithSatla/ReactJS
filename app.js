3e6b355c1323459897ac84f82030993c94633d05
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-06 11:30:34 +0530

    React Functional Components

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const heading = React.createElement(
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+////////////////////////  ******** ////////////////////////
+
 //Creating React element using JSX.
 
 const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
@@ -19,3 +21,21 @@ const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
 
 // root.render(heading);
 root.render(jsxHeading);
+
+////////////////////////  ******** ////////////////////////
+
+// Functional Component : A Functional component is a normal JS function which returns a JSX Element i.e React Element.
+
+const TutorName = () => <h1> By Ranjith Satla</h1>;
+
+const ReactTopic = () => {
+  return (
+    <div className="container">
+      <h1 id="heading"> Topic on React Functional Component </h1>
+      {/* Component Composition : Composing One Component in another Component */}
+      <TutorName />
+    </div>
+  );
+};
+
+root.render(<ReactTopic />);
