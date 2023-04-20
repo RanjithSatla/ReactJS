27cb91f3dfc7d4a19382ac778e1a2ecca801dfda
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-20 11:59:16 +0530

    About & Contact Pagesz

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>We are all yours</h1>
+      <p>This is a place where you satisfy your belly</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Components/Contact.js
@@ -0,0 +1,5 @@
+const Contact = () => {
+  return <h1>Please to mail us : foodvilla@gmail.com</h1>;
+};
+
+export default Contact;
