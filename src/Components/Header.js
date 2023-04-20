f55c804489c5989a45f846675560cf4ae02e3b1f
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-20 12:01:29 +0530

    Dynamic navigation using Link

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isLoggedInUser, setIsLoggedInUser] = useState(false);
@@ -19,10 +20,18 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About us</li>
-          <li>Contactus</li>
-          <li>Cart</li>
+          <li>
+            <Link to={"/"}> Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About us</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact Us</Link>
+          </li>
+          <li>
+            <Link to={"./cart"}>Cart</Link>
+          </li>
         </ul>
       </div>
       {isLoggedInUser ? (
