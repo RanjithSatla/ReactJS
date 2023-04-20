2440998005dd6d740ec782c07a64b322e3ed8108
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-20 12:00:41 +0530

    Fix UI issue after Link

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -6,7 +6,9 @@ const RestaurantCard = (props) => {
     <div className="res-card">
       <img className="res-image" src={CDN_URL + cloudinaryImageId}></img>
       <div className="res-info">
-        <h3>{name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}</h3>
+        <h3 className=".color-rest">
+          {name.charAt(0).toUpperCase() + name.slice(1).toLowerCase()}
+        </h3>
         <h4> {cuisines.join(", ")} </h4>
         <h4> {avgRating}⭐</h4>
         <h4> {deliveryTime}mins 🕑</h4>
