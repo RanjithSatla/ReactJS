54b4e82d1b9e251e63922168153f83cf2c602134
Ranjith Satla <ranjithsatla327@gmail.com>
2023-04-20 12:03:23 +0530

    Route configuration using react-router-dom

--- src/app.js
+++ src/app.js
@@ -1,17 +1,48 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
+import About from "./About";
+import Contact from "./Components/Contact";
+import RestaurantMenu from "./Components/RestaurantMenu";
+import Error from "./Components/Error";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 const AppLayOut = () => {
   return (
     <div>
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayOut />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "restaurant/:id",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayOut />);
+root.render(<RouterProvider router={appRouter} />);
