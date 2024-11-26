c9cecd0d2f0ba64df2e674e6834b3efaaeb67fee
RanjithSatla <ranjithsatla327@gmail.com>
2024-11-26 11:09:04 +0530

    Creating Nested Elements in React

--- Basics/App.js
+++ Basics/App.js
@@ -1,6 +1,10 @@
-const heading =  React.createElement("h1",{id:"heading"},"Hello World form React!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(heading);  // Heading is a React Element which is an JS Object.
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World form React!"
+);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(heading); // Heading is a React Element which is an JS Object.
 
 /* OP :
 {
@@ -45,4 +49,34 @@ _source
     Object
 */
 
-root.render(heading); // render method takes this JS object and does the Job of inserting in root in DOM.
+// root.render(heading); // render method takes this JS object and does the Job of inserting in root in DOM.
+
+// Nested Elements :
+
+/**
+ * <div id="parent">
+ *    <div id="children1">
+ *        <h1>I'm an h1 tag</h1>
+ *        <h2>I'm an h1 tag</h2>
+ *    </div>
+ * <div id="children2">
+ *        <h1>I'm an h1 tag</h1>
+ *        <h2>I'm an h1 tag</h2>
+ *    </div>
+ * </div>
+ *
+ */
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "children1" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm and h2 tag"),
+  ]),
+  React.createElement("div", { id: "children2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm and h2 tag"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
